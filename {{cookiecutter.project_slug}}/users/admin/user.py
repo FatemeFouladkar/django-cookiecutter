24dcde5b6be4d3752c0a916dc8f97d852e954120
fatemefouladkar <fatemeflk@chmail.ir>
2023-08-09 19:59:04 +0330

    add username type

--- {{cookiecutter.project_slug}}/users/admin/user.py
+++ {{cookiecutter.project_slug}}/users/admin/user.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from users.models.user import User
 
@@ -7,15 +8,35 @@ from users.models.user import User
 @admin.register(User)
 class UserAdmin(UserAdmin):
     model = User
-    list_display = ('id', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+    list_display = ('id', '{{ cookiecutter.username_type }}', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
     list_filter = ('is_active', 'is_staff', 'is_superuser')
-    search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('id',)
+    search_fields = ('{{ cookiecutter.username_type }}', 'first_name', 'last_name')
     
     fieldsets = (
+        {% if cookiecutter.username_type == "email" %}
         (None, {"fields": ("email", "password")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+        (_("Personal info"), {"fields": ("name",)}),
+        {% else %}
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("name", "email")}),
+        {% endif %}
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
+
+    {% if cookiecutter.username_type == "email" %}
+    ordering = ["id"]
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
@@ -25,3 +46,4 @@ class UserAdmin(UserAdmin):
             )}
         ),
     )
+    {% endif %}
--- {{cookiecutter.project_slug}}/users/models/user.py
+++ {{cookiecutter.project_slug}}/users/models/user.py
@@ -6,13 +6,19 @@ from users.managers.user import UserManager
 
 
 class User(AbstractUser):
-    username = None
+    first_name = None  # type: ignore
+    last_name = None  # type: ignore
+    {%- if cookiecutter.username_type == "email" %}
     email = models.EmailField(_("email address"), unique=True)
+    username = None
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+    {%- endif %}
 
     def __str__(self):
         return f'{self.email} -#{self.id}'
+    
+
