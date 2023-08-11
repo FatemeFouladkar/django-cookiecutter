ba969f964cce57aca1ea004697ea0b2c75d70696
Fateme Fouladkar <73009437+FatemeFouladkar@users.noreply.github.com>
2023-08-11 13:25:02 +0330

    Update settings.py

--- {{cookiecutter.project_slug}}/config/settings.py
+++ {{cookiecutter.project_slug}}/config/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': config('POSTGRES_HOST'),
-        'PORT': '5432',
+        'PORT': config('POSTGRES_PORT'),
     }
 }
 
