e6f35f08e3ea019d90b9ed952c873f1116c62530
Fateme Fouladkar <73009437+FatemeFouladkar@users.noreply.github.com>
2023-08-11 13:21:16 +0330

    Update urls.py

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+{% if cookiecutter.use_simple_jwt == 'y' %}
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+{% endif %}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
