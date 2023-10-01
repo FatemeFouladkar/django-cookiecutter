5bdbcbc7cc5829a33ae8a80a87d122c1736b30e3
Fateme Fouladkar <fatemefuoladkar@gmail.com>
2023-10-01 12:36:55 +0330

    fix issue in dockerfile for superuser variables

--- {{cookiecutter.project_slug}}/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-chmod +x /code/wait-for-it.sh
-./wait-for-it.sh db:5432
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py createsuperuser --noinput
-python manage.py collectstatic --noinput
-
-exec "$@"
