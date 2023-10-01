d26a6ec34acda6dfcde9c062cf0086d3323b3d74
Fateme Fouladkar <73009437+FatemeFouladkar@users.noreply.github.com>
2023-10-01 12:24:25 +0330

    Add createsuperuser to entrypoint.sh

--- {{cookiecutter.project_slug}}/entrypoint.sh
+++ {{cookiecutter.project_slug}}/entrypoint.sh
@@ -2,6 +2,7 @@ chmod +x /code/wait-for-it.sh
 ./wait-for-it.sh db:5432
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput
 
 exec "$@"
