e6a88d7a6da9ee4a8e13979cfaedfb1d879d4e92
fatemefouladkar <fatemeflk@chmail.ir>
2023-11-10 12:52:44 +0330

    update .env file

--- {{cookiecutter.project_slug}}/config/settings.py
+++ {{cookiecutter.project_slug}}/config/settings.py
@@ -183,7 +183,7 @@ EMAIL_BACKEND = config(
     default="django.core.mail.backends.smtp.EmailBackend",
 )
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD') 
 EMAIL_PORT = config('EMAIL_HOST_PORT', 587)
